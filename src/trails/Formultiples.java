75442fd760648f82391b5144db4ce8d28833d3b7
tejar <tejareddy5214@gmail.com>
2025-06-18 11:15:31 +0530

    forMultiples changes in print statement again

--- src/trails/Formultiples.java
+++ src/trails/Formultiples.java
@@ -3,7 +3,7 @@ package trails;
 public class Formultiples {
     public static void main(String[] args){
         for (int i=1;i<=10;i++){
-            System.out.println("5 * "+ i +" ====== "+5*i);
+            System.out.println("5 * "+ i +" = "+5*i);
         }
     }
 }
